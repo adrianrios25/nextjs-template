dfa82e6a99ce15885823311f0176ffdb51db335a
Rios <AURios@eastwestbanker.com>
2024-10-09 10:46:52 +0800

    added test api

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
+	console.log(request);
 	return NextResponse.json({ message: 'Backoffice Test' }, { status: 200 });
 }
