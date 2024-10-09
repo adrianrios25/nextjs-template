2ca46086fb79fd8c2e693273e879346d136f9c57
Rios <AURios@eastwestbanker.com>
2024-10-09 15:24:28 +0800

    environment variable

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,8 +3,14 @@ import { NextResponse, NextRequest } from 'next/server';
 export async function GET(req: NextRequest) {
 	const xForwarededFor = req.headers.get('x-forwarded-for') ?? '';
 	console.log(xForwarededFor);
+	console.log(req);
 	return NextResponse.json(
-		{ message: 'Backoffice Test', value: process.env.DB_NAME, xForwarededFor },
+		{
+			message: 'Backoffice Test',
+			value: process.env.DB_NAME,
+			xForwarededFor,
+			TESTENVVARIABLE: process.env.TESTENVVARIABLE,
+		},
 		{ status: 200 }
 	);
 }
