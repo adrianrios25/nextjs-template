de924a5237ae02da0878a81695930b7aa014dec2
Rios <AURios@eastwestbanker.com>
2024-10-09 11:09:03 +0800

    add env file

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
-	console.log(process.env.DB_NAME);
+	console.log(request);
 	return NextResponse.json(
 		{ message: 'Backoffice Test', value: process.env.DB_NAME },
 		{ status: 200 }
